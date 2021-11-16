2fe73039889b33d39ebed91329e2ae91482cc5b5
wonseopK <wonseop77@daum.net>
2021-11-16 15:58:08 +0900

    base

---
+++ src/main/java/boot/day1104/shop/GitPractice3Application.java
@@ -0,0 +1,13 @@
+package boot.day1104.shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitPractice3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitPractice3Application.class, args);
+	}
+
+}
---
+++ src/test/java/boot/day1104/shop/GitPractice3ApplicationTests.java
@@ -0,0 +1,13 @@
+package boot.day1104.shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitPractice3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
